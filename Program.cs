6aee040d3d775b96a8ed8d3cc1b0760172a987a1
Mate Cserhati <mate.cserhati03@gmail.com>
2022-09-30 23:43:22 +0200

    v2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace passwordGenerator
 {
@@ -6,7 +7,32 @@ namespace passwordGenerator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+                Console.Write("Kérlek add meg, hogy milyen hosszú legyen a jelszó! ");
+                Console.WriteLine($"Az ön jelszava: {PasswordGenerator(Int32.Parse(Console.ReadLine()))}");
+                Console.ReadLine();
+
+        }
+
+        static string PasswordGenerator(int length)
+        {
+            if (length <= 1)
+            {
+                throw new Exception("Nem lehet kisebb vagy egyenlő mint 1");
+            }
+
+            string chars = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%&";
+
+            Random random = new Random();
+
+            string password = String.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                if (random.Next(0, 2) == 0)
+                    password += chars[random.Next(0, chars.Length)].ToString().ToUpper();
+                else
+                    password += chars[random.Next(0, chars.Length)];
+            }
+            return password;
         }
     }
 }
